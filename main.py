de36b31f1d949b93c9867ff80bb5e699e1625cfe
AelaliMazeratti <aelalimazeratti@gmail.com>
2026-08-27 21:46:42 +0400

    daily stats command

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
-from settings import *
 from dotenv import load_dotenv
+from settings import *
 import json
-
+import random
 
 load_dotenv()
+os.makedirs('data', exist_ok=True)
 
 token = os.getenv("DISCORD_TOKEN")
 if token is None:
-    raise RuntimeError("DISCORD_TOKEN is not set")
+    raise RuntimeError("DISCORD_TOKEN is not set. Please set it in the .env file.")
 
 @bot.event
 async def on_ready():
@@ -16,9 +17,42 @@ async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
 
-@bot.command()
+@bot.command() # sends "Pong!" to the channel where the command was invoked
 async def ping(ctx):
-    # sends "Pong!" to the channel where the command was invoked
     await ctx.send('Pong!')
+
+@bot.command() # sends daily stats to the channel where the command was invoked
+async def dailystats(ctx):
+    if os.path.exists('data/daily_stats.json'): # if the file exists, load the data from it
+        with open('data/daily_stats.json', 'r') as file:
+            daily_stats = json.load(file)
+    else: # if the file does not exist, create an empty dictionary
+        daily_stats = {}
+
+    user_id = str(ctx.author.id)
+    today = datetime.datetime.now(ZoneInfo("Asia/Yerevan")).date().isoformat()
+
+    if user_id not in daily_stats or daily_stats[user_id]["last_used"] != today: # user has already used the command today
+        cringe = random.randint(1, 100)
+        sus = random.randint(1, 100)
+        
+        daily_stats[user_id] = {
+            "last_used": today,
+            "stats": {
+                "cringe": cringe,
+                "sus": sus
+                }
+            }
+        
+        with open('data/daily_stats.json', 'w') as file:
+            json.dump(daily_stats, file, indent=4)
+
+    cringe = daily_stats[user_id]["stats"]["cringe"]
+    sus = daily_stats[user_id]["stats"]["sus"]
+
+    await ctx.send(f"{ctx.author.mention}'s daily stats:\n"
+                       f"Cringe: {cringe}%\n"
+                       f"Sus: {sus}%"
+                       )
 
 bot.run(token)
--- settings.py
+++ settings.py
@@ -4,4 +4,6 @@ import datetime
 from zoneinfo import ZoneInfo
 
 intents = discord.Intents(messages=True, guilds=True, message_content=True, members=True) # bot permissions
-bot = commands.Bot(command_prefix="!", intents=intents) # bot command prefix
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents) # bot command prefix
+
+now = datetime.datetime.now(ZoneInfo("Asia/Yerevan")) # timezone selection
