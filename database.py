86e2958f221adfcceaafe49830b6051c84cb2c9d
AelaliMazeratti <aelalimazeratti@gmail.com>
2026-09-03 02:46:43 +0400

    create module

--- database.py
+++ database.py
@@ -1,7 +1,36 @@
 import sqlite3
+import os
+import discord
+from datetime import datetime
+from settings import TIMEZONE
+import json
 
-def db_init():
-    con = sqlite3.connect('data/database.db')
+# returns the path to the guild's data directory, creating it if it doesn't exist
+def get_guild_data_path(guild_id: int, root: str = "data") -> str:
+    path = os.path.join(root, f"guild_{guild_id}")
+    os.makedirs(path, exist_ok=True)
+    return path
+
+# creates or updates the metadata.json file for a guild
+def update_guild_metadata(guild: discord.Guild, root: str = "data") -> None:
+    path = get_guild_data_path(guild.id, root)
+    metadata_file = os.path.join(path, "metadata.json")
+
+    metadata = {
+        "name": guild.name,
+        "id": guild.id
+    }
+
+    with open(metadata_file, "w") as file:
+        json.dump(metadata, file, indent=4)
+
+# initializes the database and create relevant tables if they don't exist
+def db_init(guild, root="data") -> None:
+    guild_id = guild.id
+    path = get_guild_data_path(guild_id, root)  # ensure the guild data directory exists
+    update_guild_metadata(guild, root)  # update the metadata.json file
+    name = f"guild_{guild_id}.db"
+    con = sqlite3.connect(os.path.join(path, name))
     cur = con.cursor()
 
     # create the daily_stats table if it doesn't exist
@@ -21,3 +50,6 @@ def db_init():
         raise RuntimeError("Failed to create or access the daily_stats table in the database. Please check the database file and ensure it is accessible.")
 
     con.close()
+
+def get_today() -> str:
+    return datetime.now(TIMEZONE).date().isoformat()
