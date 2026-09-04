1d9ae62ae7f0147d6a60e095f470d2c2cc96e3fb
AelaliMazeratti <aelalimazeratti@gmail.com>
2026-09-04 19:53:45 +0400

    updated database int

--- database.py
+++ database.py
@@ -24,14 +24,30 @@ def update_guild_metadata(guild: discord.Guild, root: str = "data") -> None:
     with open(metadata_file, "w") as file:
         json.dump(metadata, file, indent=4)
 
+# create campaign, NOT FINISHED YET
+def create_campaign(guild_id: int, campaign_name: str, root: str = "data") -> None:
+    path = get_guild_data_path(guild_id, root)
+    campaign_path = os.path.join(path, "campaigns", campaign_name)
+    os.makedirs(campaign_path, exist_ok=True)
+    cleaned_campaign_name = campaign_name.replace(" ", "_").lower()
+    db_file = os.path.join(campaign_path, f"{cleaned_campaign_name}.db")
+
 # initializes the database and create relevant tables if they don't exist
-def db_init(guild, root="data") -> None:
+def db_init(guild, root: str = "data") -> None:
     guild_id = guild.id
     path = get_guild_data_path(guild_id, root)  # ensure the guild data directory exists
     update_guild_metadata(guild, root)  # update the metadata.json file
     name = f"guild_{guild_id}.db"
     con = sqlite3.connect(os.path.join(path, name))
     cur = con.cursor()
+
+    # create the campaigns table if it doesn't exist
+    cur.execute("CREATE TABLE IF NOT EXISTS campaigns (" \
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+            "name TEXT NOT NULL," \
+            "game_system TEXT NOT NULL" \
+        ")"
+    )
 
     # create the daily_stats table if it doesn't exist
     cur.execute("CREATE TABLE IF NOT EXISTS daily_stats (" \
@@ -44,12 +60,18 @@ def db_init(guild, root="data") -> None:
     )
     con.commit()
 
-    # verify that the table was created successfully
-    res = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='daily_stats'")
-    if res.fetchone() is None:
+    # verify that the campaigns table was created successfully
+    campaigns = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='campaigns'")
+    if campaigns.fetchone() is None:
+        raise RuntimeError("Failed to create or access the campaigns table in the database. Please check the database file and ensure it is accessible.")
+
+    # verify that the daily_stats table was created successfully
+    daily_stats = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='daily_stats'")
+    if daily_stats.fetchone() is None:
         raise RuntimeError("Failed to create or access the daily_stats table in the database. Please check the database file and ensure it is accessible.")
 
     con.close()
 
+# returns the current date in the specified timezone as a string in ISO format
 def get_today() -> str:
     return datetime.now(TIMEZONE).date().isoformat()
